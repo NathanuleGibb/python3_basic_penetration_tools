6e93157bcf671e8d6240840407348272db32ab58
NathanuleGibb <113156523+NathanuleGibb@users.noreply.github.com>
2022-10-27 16:55:51 +1100

    Add files via upload

---
+++ Basic_Random_Wordlists_Gen.py
@@ -0,0 +1,14 @@
+import string
+import random
+
+passcount = int(input('how many passwords? :'))
+password = string.ascii_letters + string.digits
+char = ''
+file = open("wordlists.txt", "w")
+for count in range(passcount):
+    for x in random.sample(password,random.randint(8,8)):
+        char+=x
+    file.write(char+'\n')
+    char=''
+file.close()
+print('pass list is ready')
